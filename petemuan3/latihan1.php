295edfcfbdbeba6878b054486c814657a93e1cec
ibnudirsan <ibnudirsan@gmail.com>
2020-08-16 10:25:53 +0700

    pertemuan3

---
+++ petemuan3/latihan1.php
@@ -0,0 +1,29 @@
+<?php
+// ternary
+
+    // Pengulangan
+    // for
+    // while
+    // do.. while
+    // foreach : pengulangan khusus Array
+
+    // Dengan Pengulangan For
+    // for ( $i = 0; $i < 5; $i++ ){
+    //     echo "Hallo Word <br>";
+    // }
+
+    // Dengan Pengulangan while //tidak dikerjakan jika tidak memenuhi kondisi
+    // $i = 0;
+    // while ( $i< 5  ){
+    //     echo "Hello Word <br>";
+    // $i++;
+    // }
+
+    // Dengan Pengulangan do.. while // dikerjakan minimal sekali jika tidak memenuhi kondisi
+    // $i=10;
+    // do {
+    //     echo "Hollo Word <br>";
+    // $i++;
+    // } while ( $i < 5 );
+
+?>
---
+++ petemuan3/latihan4.php
@@ -0,0 +1,18 @@
+<?php
+    // Pengkondisian atau pecabangan.
+    // if else
+    // if else if else
+    // ternary
+    // switch
+
+    // Untuk yang if
+    $x = 2;
+    if($x < 20){
+        echo "benar";
+    } else if ( $x == 30){
+        echo "Yes";
+    }
+    else {
+        echo "salah";
+    }
+?>
